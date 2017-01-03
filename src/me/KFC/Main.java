41a331fdb158886f75431fe842cdf7a3fdd8beb8
CoderBrewings <brewingsmc@gmail.com>
2017-01-03 12:35:35 +1100

    testing hello somebody help me

--- src/me/KFC/Main.java
+++ src/me/KFC/Main.java
@@ -4,7 +4,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
-	//test hello
+	//test hello god help me
 	public void onEnable() {
 		this.getCommand("twitter").setExecutor(new Help(this));
 	}
