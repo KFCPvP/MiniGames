c307dadc3f36f961dab2d19dd65ec6382b62cc60
CoderBrewings <brewingsmc@gmail.com>
2017-01-03 12:34:31 +1100

    testing 123

--- src/me/KFC/Main.java
+++ src/me/KFC/Main.java
@@ -4,7 +4,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
-	//test
+	//test hello
 	public void onEnable() {
 		this.getCommand("twitter").setExecutor(new Help(this));
 	}
